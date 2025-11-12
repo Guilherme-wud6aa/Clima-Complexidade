f4359215d69e0108fbcfeb95e6aed50e3e4767e1
Guilherme Fontes <132164831+Wud6aa@users.noreply.github.com>
2025-11-12 17:53:49 -0300

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import requests
+from datetime import date, timedelta
+
+latitude = -10.96
+longitude = -37.05
+datas = []
+hoje = date.today()
+next_day = hoje + timedelta(days=1)
+next_week = hoje + timedelta(days=8)
+
+
+url = (
+    f"https://api.open-meteo.com/v1/forecast?"
+    f"latitude={latitude}&longitude={longitude}"
+    f"&start_date={next_day}&end_date={next_week}"
+    f"&daily=temperature_2m_max,temperature_2m_min,precipitation_sum"
+    f"&timezone=America/Sao_Paulo"
+)
+
+response = requests.get(url)
+data = response.json()
+
+datas = data["daily"]["time"]
+temp_max = data["daily"]["temperature_2m_max"]
+temp_min = data["daily"]["temperature_2m_min"]
+precip = data["daily"]["precipitation_sum"]
+
+print("Dados meteorológicos de Aracaju (SE) - Ontem e Hoje:\n")
+for i in range(len(datas)):
+    print(f"Data: {datas[i]}")
+    print(f"Temperatura máxima: {temp_max[i]} °C")
+    print(f"Temperatura mínima: {temp_min[i]} °C")
+    print(f"Precipitação: {precip[i]} mm\n")
